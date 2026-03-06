8fb063c5a641e4079ccfc4e575cfb9aada8d2e52
amelinaahmeti <amelina.ahmeti@fscj.edu>
2026-03-06 14:45:34 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import random
+import time
 
-st.title("Echo Bot")
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -9,21 +24,12 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-       st.markdown(message["content"])
+         st.markdown(message["content"])
 
-# React to user input
+# Accept user input
 if prompt := st.chat_input("What is up?"):
-   # Display user message in chat message container
-   st.chat_message("user").markdown(prompt)
-
-   # Add user message to chat history
-   st.session_state.messages.append({"role": "user", "content": prompt})
-
-   response = f"Echo: {prompt}"
-   # Display assistant response in chat message container
-
-   with st.chat_message("assistant"):
-        st.markdown(response)
-
-   # Add assistant response to chat history
-   st.session_state.messages.append({"role": "assistant", "content": response})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+         st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
